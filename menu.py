0f6462878aebcef02bded5cbc6fb1643fc40c894
BrunoMirandaR <Br.mirandar@duocuc.cl>
2024-06-13 21:16:04 -0400

    Update and rename menu.md to menu.py
    Se cambio el archivo a formato python y se agregaron las librerias pertinentes (de momento solo funciona carta)

---
+++ menu.py
@@ -0,0 +1,23 @@
+#menu.py
+import sys
+import Funciones_del_menu as lb
+#solo_funciona_carta
+x = 0
+while x != 4:
+    x = int(input(""""------------- Hola, Bienvenido a MIRANDAS -------------
+    Escriba el numero de la opcione que desea usar: 
+    1) Ver carta
+    2) Comprar
+    3) Miembros VIP
+    4) Salir
+     """))
+    if x == 1:
+        lb.carta()
+    elif x == 2:
+        lb.comprar()
+    elif x == 3:
+        lb.miembros()
+    elif x == 4:
+        sys.exit()
+    else:
+        print("Opcion no valida, intentelo de nuevo")
