8b505e5e465418439cce47e39152f3c6f333efea
uFeiku <die.mirandab@duocuc.cl>
2024-06-17 09:14:05 -0400

    Update menu.py

--- menu.py
+++ menu.py
@@ -2,22 +2,26 @@
 import sys
 import Funciones_del_menu as lb
 #solo_funciona_carta
-x = 0
-print("------------- Hola, Bienvenido a MIRANDAS -------------")
-while x != 4:
-    x = int(input("""Escriba el numero de la opcione que desea usar: 
-    1) Ver carta
-    2) Comprar
-    3) Miembros VIP
-    4) Salir
-     """))
-    if x == 1:
-        lb.carta()
-    elif x == 2:
-        lb.ventas()
-    elif x == 3:
-        lb.sesion()
-    elif x == 4:
-        sys.exit()
-    else:
-        print("Opcion no valida, intentelo de nuevo")
+
+def menu():
+    x = 0
+    print("------------- Hola, Bienvenido a MIRANDAS -------------")
+    while x != 4:
+        x = int(input("""Escriba el numero de la opcione que desea usar: 
+        1) Ver carta
+        2) Comprar
+        3) Miembros VIP
+        4) Salir
+        """))
+        if x == 1:
+            lb.carta()
+        elif x == 2:
+            lb.ventas()
+        elif x == 3:
+            lb.sesion()
+        elif x == 4:
+            sys.exit()
+        else:
+            print("Opcion no valida, intentelo de nuevo")
+
+lb.sesion()
