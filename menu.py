b8878484e678ea9d1ec7aee9b443652223d1a1bf
uFeiku <die.mirandab@duocuc.cl>
2024-06-26 21:34:51 -0400

    Update menu.py
    optimización de menu y añadí el lb.sesion que mató al bruno 👎

--- menu.py
+++ menu.py
@@ -1,22 +1,24 @@
 #menu.py
-import sys
 import Funciones_del_menu as lb
-#funciona bien :D
+import sys
+
+lb.sesion()
 x = 0
-print("------------- Hola, Bienvenido a MIRANDAS -------------")
+print("------------- Bienvenido a MIRANDAS -------------")
 while x != 4:
+    print("")
     x = int(input("""Escriba el numero de la opcione que desea usar: 
     1) Ver carta
     2) Comprar
     3) Miembros VIP
     4) Salir
-    Elija:"""))
+    Elija: """))
     if x == 1:
         lb.carta()
     elif x == 2:
         lb.ventas()
     elif x == 3:
-        lb.sesion()
+        lb.vip()
     elif x == 4:
         sys.exit()
     else:
