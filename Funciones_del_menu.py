c07589ea90f06ef91b1c131fef93c721b17459b2
uFeiku <die.mirandab@duocuc.cl>
2024-06-26 21:35:47 -0400

    Update Funciones_del_menu.py
    Lo dejé mas bonito c: Y le añadí mejoras

--- Funciones_del_menu.py
+++ Funciones_del_menu.py
@@ -1,6 +1,8 @@
 #Beta del menu
 import sys
+desc = False
 def carta():
+
     print("""--------------------- Carta ---------------------
     1. Completo Italiano $3000
     Ingredientes: Palta, tomate y mayo.
@@ -29,43 +31,80 @@ def carta():
     ------------------------------------------------------------------------------------
     10. Papas fritas $2000
     ------------------------------------------------------------------------------------""")
-
+    
 def ventas ():
+    global desc
+    global comida
+    global totaliva
     total = 0
+    comida = ["Completo Italiano $3000","Completo Aleman $3500","Completo Dinamico $4000","Completo Chacarero $4500","Completo Barros Luco $5000","Completo Bruno $5500","Completo Diego $6000","Bebida 600ml $1000","Jugo natural $1500","Papas fritas $2000"]
     while True:
         try:
             try:
                     pedido = int(input("Ingrese el numero de su pedido: "))
                     if pedido == 1:
                         total += 3000
+                        print("Ha seleccionado\n",comida[0])
+
                     elif pedido == 2:
                         total += 3500
+                        print("Ha seleccionado\n",comida[1])
                     elif pedido == 3:
                         total += 4000
+                        print("Ha seleccionado\n",comida[2])
                     elif pedido == 4:
                         total += 4500
+                        print("Ha seleccionado\n",comida[3])
                     elif pedido == 5:
                         total += 5000
+                        print("Ha seleccionado\n",comida[4])
                     elif pedido == 6:
                         total += 5500
+                        print("Ha seleccionado\n",comida[5])
                     elif pedido == 7:
                         total += 6000
+                        print("Ha seleccionado\n",comida[6])
                     elif pedido == 8:
                         total += 1000
+                        print("Ha seleccionado\n",comida[7])
                     elif pedido == 9:
                         total += 1500
+                        print("Ha seleccionado\n",comida[8])
                     elif pedido == 10:
                         total += 2000
+                        print("Ha seleccionado\n",comida[9])
                     else:
                         print("Pedido no valido")
                     while True:
                         try:
+                            print("")
                             continuar = input("Desea seguir pidiendo? (si/no): ")
                             if continuar == "si":
                                 break
                             elif continuar == "no":
-                                print("Total a pagar: $", total * 1.19)
-                                exit()
+                                if desc==True: #CON DESCUENTO ACTIVO
+
+                                    monto = total
+                                    subtotal = monto
+                                    IVA = total * 0.19
+                                    total = total * 0.81
+                                    monto = monto * 0.85
+                                    print("Precio: $",total)
+                                    print("IVA: $",IVA)
+                                    print("SubTotal:$",subtotal)
+                                    print("Descuento: $ -15% ")
+                                    print("Total a pagar: $",monto)
+                                    exit()
+
+
+                                elif desc==False: #CON DESCUENTO DESACTIVADO
+                                    monto = total
+                                    IVA = total * 0.19
+                                    total = total * 0.81
+                                    print("Precio: $",total )
+                                    print("IVA: $",IVA)
+                                    print("Total a pagar: $",monto)
+                                    exit()
                             else:
                                 print("Respuesta no valida")
                             continue
@@ -78,36 +117,37 @@ def ventas ():
             print("Pedido no valido")
         continue
 
-def menu():
-    x = 0
-    print("------------- Hola, Bienvenido a MIRANDAS -------------")
-    while x != 4:
-        x = int(input("""Escriba el numero de la opcione que desea usar: 
-        1) Ver carta
-        2) Comprar
-        3) Miembros VIP
-        4) Salir
-        """))
-        if x == 1:
-            carta()
-        elif x == 2:
-            ventas()
-        elif x == 3:
-            sesion()
-        elif x == 4:
-            sys.exit()
-        else:
-            print("Opcion no valida, intentelo de nuevo")
+def vip():
+    global desc
+    
 
+    print("Vip consiste en tener un 15% de descuento en cada compra por un monto de 4.000 al mes")
+    print("")
+    salidavip = False
+    while salidavip == False:
+        vip=str(input("""Desea pagar para obtener su descuento (si/no)
+        Su eleccion: """))
+        if vip == "si":
+            desc=True
+            print("   Usuario vip $3240")
+            print("      Iva(19%) $760")
+            print("Total a pagar: $4000")
+            print("")
+            salidavip = True
+        elif vip == "no":
+            print("")
+            salidavip = True
+        else:
+            print("Opcion no valida.")
 global rut
 def sesion():
-    intento = 4
+    intento = 3
     rut = -5
-
-    while rut < 0 or rut < 1000000:
+    
+    while rut < 0 or rut < 10000000: #se coloca el valor para un rut valido
         rut=int(input("Ingrese su rut sin guin ni puntos (EJ:123456789): ")[0:9])
 
-        if rut < 0 or rut < 1000000:
+        if rut < 0 or rut < 10000000:
             intento = intento - 1
             print("Rut invalido, intentelo nuevamente")
             print (f"Le quedan: {intento} intentos.")
@@ -117,7 +157,6 @@ def sesion():
                 print("Se superaron los intentos, intentelo más tarde.")
                 sys.exit()
     
-
         else:
             contraseña=input("Ingrese su contraseña: ")
             break
@@ -132,6 +171,6 @@ def sesion():
         Rut: {rut_cambiado}
         Contraseña: {contraseña}
             """)
-    menu()
     
     
+
