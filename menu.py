37da3a13f67939fd52ae249d5a76a22ea3bf0438
BrunoMirandaR <Br.mirandar@duocuc.cl>
2024-06-26 20:32:11 -0400

    Update menu.py
    ya funciona el menu, se quito el def

--- menu.py
+++ menu.py
@@ -1,27 +1,23 @@
 #menu.py
 import sys
 import Funciones_del_menu as lb
-#solo_funciona_carta
-
-def menu():
-    x = 0
-    print("------------- Hola, Bienvenido a MIRANDAS -------------")
-    while x != 4:
-        x = int(input("""Escriba el numero de la opcione que desea usar: 
-        1) Ver carta
-        2) Comprar
-        3) Miembros VIP
-        4) Salir
-        """))
-        if x == 1:
-            lb.carta()
-        elif x == 2:
-            lb.ventas()
-        elif x == 3:
-            lb.sesion()
-        elif x == 4:
-            sys.exit()
-        else:
-            print("Opcion no valida, intentelo de nuevo")
-
-lb.sesion()
+#funciona bien :D
+x = 0
+print("------------- Hola, Bienvenido a MIRANDAS -------------")
+while x != 4:
+    x = int(input("""Escriba el numero de la opcione que desea usar: 
+    1) Ver carta
+    2) Comprar
+    3) Miembros VIP
+    4) Salir
+    Elija:"""))
+    if x == 1:
+        lb.carta()
+    elif x == 2:
+        lb.ventas()
+    elif x == 3:
+        lb.sesion()
+    elif x == 4:
+        sys.exit()
+    else:
+        print("Opcion no valida, intentelo de nuevo")
